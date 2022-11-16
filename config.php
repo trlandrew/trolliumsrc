0989cc4383eaf6388d0eeb35e746cd08d4e7a970
trlandrew <118364011+trlandrew@users.noreply.github.com>
2022-11-16 19:54:56 +0600

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'mysql.ct8.pl');
 define('DB_USERNAME', 'm29571_Andrew');
-define('DB_PASSWORD', 'Workcord3');
+define('DB_PASSWORD', 'makebyurselflol');
 define('DB_NAME', 'm29571_trollium');
   
   $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
